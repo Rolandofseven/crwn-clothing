7a486aea305f61c5642294fdd0d9cc7faa7892e4
Piggott <Roland.Piggott@dnvgl.com>
2019-09-07 19:02:08 +0100

    db call

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,8 +14,21 @@ const config = {
 export const createUserProfileDocument = async (userAuth, additonalData) => {
     if (!userAuth) return;
 
-    const userRef = firestore.doc('users/sdfd');
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
     const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({ displayName, email, createdAt, ...additonalData })
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+
+        return userRef;
+    }
 
     console.log(snapShot);
 }
